f63d16adadf7a9b7fe0a63313091e599bda6f96c
Miguel Carmona <miguelcar18@gmail.com>
2014-09-10 20:18:08 -0430

    JMS en AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,6 +32,9 @@ class AppKernel extends Kernel
             new Project\BackBundle\ProjectBackBundle(),
             new Project\UserBundle\ProjectUserBundle(),
             new Project\FrontBundle\ProjectFrontBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
