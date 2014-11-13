e95e2060e6accbad9606453ebf7ea5fd433b64bd
Jonathan Araul <jonathan@firstimageus.com>
2014-11-13 11:00:25 -0430

    Actualizacion security controller

--- src/Project/UserBundle/Controller/SecurityController.php
+++ src/Project/UserBundle/Controller/SecurityController.php
@@ -74,11 +74,11 @@ class SecurityController extends ContainerAware
     protected function renderLogin(array $data)
     {
         $variable ='';
-          $ruta = 'FOSUserBundle:Security:login.html.%s';
+          $ruta = 'FOSUserBundle:Security:login.html.twig';
         //$ruta = 'FOSUserBundle:Security:login2.html.%s';
 
 
-        $template = sprintf($ruta, $this->container->getParameter('fos_user.template.engine'));
+        $template = sprintf($ruta);
         //$template = sprintf($ruta);
 
         return $this->container->get('templating')->renderResponse($template, $data);
