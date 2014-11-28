fe1ce48d3e3e6627721ad7963e65c3b64b430bfb
Miguel Carmona <miguelcar18@gmail.com>
2014-11-28 10:21:14 -0430

    eliminar carpeta ckeditor

--- ckeditor/filemanager/confic.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("C4d28b848", "../web/images/"); // For example ../../userfiles/
-define("C70d7bd0f", "localhost/efimeras/ckeditor/"); // Absolute path for example http://www.mysite.com
-?>
