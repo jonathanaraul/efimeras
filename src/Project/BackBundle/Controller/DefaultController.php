94dbc4cd8d848b32fb7a0e2eccff4654f8da5579
Jonathan.araul <jonathan.araul@gmail.com>
2015-03-24 00:48:54 -0430

    Commit prueba jonathan

--- src/Project/BackBundle/Controller/DefaultController.php
+++ src/Project/BackBundle/Controller/DefaultController.php
@@ -19,6 +19,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         //Tu haces tus cambios trabajas todos tus codigos guardas y luego vas al sourcetree	
+        // Para traer cambios solo debes darle en pull
         $user = $this->getUser();
         if($user==null) return $this->redirect($this->generateUrl('fos_user_security_login'));
  
